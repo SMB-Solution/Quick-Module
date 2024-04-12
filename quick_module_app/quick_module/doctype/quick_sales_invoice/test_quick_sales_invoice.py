3848a5ecc195e5fee36bef26362afa0eaf8a5762
kaushal.khute <kaushal@pseudocode.co>
2024-04-12 17:31:49 +0530

    quick module app

---
+++ quick_module_app/quick_module/doctype/quick_sales_invoice/test_quick_sales_invoice.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, kaushal khute and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestQuickSalesInvoice(FrappeTestCase):
+	pass
