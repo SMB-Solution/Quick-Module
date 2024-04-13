c7671a27758f1991f35fb726e82fdb08f91182a0
kaushal.khute <kaushal@pseudocode.co>
2024-04-13 16:32:49 +0530

    delected v-14 Dublicate sales invoice

--- quick_module_app/quick_module/doctype/quick_sales_invoice/test_quick_sales_invoice.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, kaushal khute and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestQuickSalesInvoice(FrappeTestCase):
-	pass
